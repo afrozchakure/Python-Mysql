615225754f03e141e6ed13e3e38a266b3c772bb2
Afroz Chakure <aaaanchakure@gmail.com>
2019-09-23 21:04:59 +0530

    Added files for selecting a single row

--- select_from_table.py
+++ select_from_table.py
@@ -11,13 +11,23 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()  # Creates an object of mydb
 
-mycursor.execute("SELECT * FROM customers;")
 
-myresult = mycursor.fetchall()  # Fetches all rows from the last executed statement (returns a list)
+### For selecting all the rows form a table
 
-for x in myresult:  # Used to display all the rows within the myresult list
-    print(x)
+# mycursor.execute("SELECT * FROM customers;")   # Displays all the rows of the table
+# myresult = mycursor.fetchall()  # Fetches all rows from the last executed statement (returns a list)
+# for x in myresult:  # Used to display all the rows within the myresult list
+#     print(x)
+# print(type(myresult))  # It is a list of values
 
-print(type(myresult))  # It is a list of values
 
+### For selecting a single row from a table with specified columns 
+
+mycursor.execute("SELECT name, address FROM customers")  # Displays only the specified columns of the table
+myresult = mycursor.fetchone()  # It will return only  the first row of the result
 print(myresult) # Displays the list of values
+print(type(myresult))  # It is a tuple with single value
+
+
+
+
