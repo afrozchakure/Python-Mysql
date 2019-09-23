6aa611100274c21726f87ace891ba77f9eaff3db
Afroz Chakure <aaaanchakure@gmail.com>
2019-09-23 20:39:05 +0530

    Added files for adding multiple rows in a Table

--- insert_values.py
+++ insert_values.py
@@ -13,10 +13,35 @@ mycursor = mydb.cursor()
 
 sql = "INSERT INTO customers (name, address) VALUES (%s, %s);"
 
-val = ("John", "Highway 21")
+### For Inserting a single row in a table
 
-mycursor.execute(sql, val)   
+# val = ("John", "Highway 21")
+
+# mycursor.execute(sql, val)  # Use it for inserting a single row in database
+
+# print(mycursor.rowcount, "Record Inserted.")
+
+### For Inserting multiple rows in a Table
+
+val = [
+  ('Peter', 'Lowstreet 4'),
+  ('Amy', 'Apple st 652'),
+  ('Hannah', 'Mountain 21'),
+  ('Michael', 'Valley 345'),
+  ('Sandy', 'Ocean blvd 2'),
+  ('Betty', 'Green Grass 1'),
+  ('Richard', 'Sky st 331'),
+  ('Susan', 'One way 98'),
+  ('Vicky', 'Yellow Garden 2'),
+  ('Ben', 'Park Lane 38'),
+  ('William', 'Central st 954'),
+  ('Chuck', 'Main Road 989'),
+  ('Viola', 'Sideway 1633')
+]
+
+mycursor.executemany(sql, val)
 
 mydb.commit()  # It is required to make changes otherwise no changes will be made
 
-print(mycursor.rowcount, "Record Inserted.")
+
+print(mycursor.rowcount, "was inserted.")
